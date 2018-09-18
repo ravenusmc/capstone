ba1571cfca67037a534e59b94d669bbafbc91acd
ravenusmc <mcuddy77@gmail.com>
2018-09-18 13:17:43 -0400

    added code to add new user to users table

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include 'view/header.php'; ?>
 <h1>Landing Page</h1>
 
+
 <a href="charity_signup.php">Customer Sign Up</a>
 <br>
 <a href='user_signup.php'>User Sign Up</a>
--- user_signup.php
+++ user_signup.php
@@ -8,15 +8,59 @@
   require('./model/database.php');
   global $db;
 
+  $message = "";
+
   //When the user hits the submit button this conditional statement checks to ensure everything is ok
   if (isset($_POST["login"])){
+    
     $firstname = filter_input(INPUT_POST, 'firstname');
     $lastName = filter_input(INPUT_POST, 'lastName');
     $email = filter_input(INPUT_POST, 'email');
     $username = filter_input(INPUT_POST, 'username');
+    $street = filter_input(INPUT_POST, 'street');
+    $town = filter_input(INPUT_POST, 'town');
+    $state = filter_input(INPUT_POST, 'state');
+    $zip = filter_input(INPUT_POST, 'zip');
+    $password = filter_input(INPUT_POST, 'password');
+    $password2 = filter_input(INPUT_POST, 'password2');
 
-    echo $username;
-  }
+    //Hashing the password 
+    $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+
+    //This SQL query checks to see if the username is in the users table.
+    $query = "SELECT * FROM users WHERE 
+              username = :username AND 
+              password = :password";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':password', $password);
+    $count = $statement->rowCount();
+    //Conditional statements based on what the query returns. 
+    if ($count > 0){
+      $message = '<label>Username Taken!</label>';
+    }else {
+      //Query to add new user to the users table
+      $query = 'INSERT INTO users
+                  (firstname, lastname, street, town, state, zip, email, username, password)
+                VALUES
+                  (:firstname, :lastname, :street, :town, :state, :zip, :email, :username, :password)'; 
+      $statement = $db->prepare($query); 
+      $statement->bindValue(':firstname', $firstname);
+      $statement->bindValue(':lastname', $lastname);
+      $statement->bindValue(':street', $street);
+      $statement->bindValue(':town', $town);
+      $statement->bindValue(':state', $state);
+      $statement->bindValue(':zip', $zip);
+      $statement->bindValue(':email', $email);
+      $statement->bindValue(':username', $username);
+      $statement->bindValue(':password', $password_hashed);
+      $statement->execute();
+      $statement->closeCursor();
+      //Message to alert user that they signed up
+      $message = '<label>User Signed Up!</label>';
+    }
+
+  }//End of if main conditional 
 
 ?>
 <?php include 'view/header.php'; ?>
@@ -67,7 +111,11 @@
     </div>
     <div class="form-group">
       <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      <input type="password" name='password' class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword2">Password</label>
+      <input type="password" name='password2' class="form-control" id="exampleInputPassword2" placeholder="Confirm Password">
     </div>
     <button type="submit" name="login" class="btn btn-primary">Submit</button>
   </form>
