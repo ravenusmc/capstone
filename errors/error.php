b7d326ce853cf4ac8609b0c7e6ac6918721d87dd
Liz <admin@elizabethmbank.info>
2018-09-28 11:08:21 -0400

    removed a line in error.php

--- errors/error.php
+++ errors/error.php
@@ -3,6 +3,6 @@ include('../view/header.php'); ?>
 <div id="main">
     <h1 class="top">Error</h1>
     <p class="first_paragraph"><?php echo $error; ?></p>
-    <a href="<?php echo $appPath ?>">Home</a>
+    
 </div>
 <?php include('../view/footer.php'); ?>
