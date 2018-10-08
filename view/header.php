a24108b7168892fab3ebcc6a89a4fccbe415dd0c
Liz <admin@elizabethmbank.info>
2018-10-08 19:48:23 -0400

    added css and working on landing.php file

--- view/header.php
+++ view/header.php
@@ -3,8 +3,10 @@
   
   //This variable will be a flag to verify if the user is currently in a session
   //or just browsing. 
-  $name = $_SESSION["username"];
-  $id = $_SESSION["user_id"];
+  //$name = $_SESSION["username"];
+  $name = 'Hello World!';
+  //$id = $_SESSION["user_id"];
+  $id = 'MyMyselfandI';
 
  
   
@@ -19,18 +21,18 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="styleshet" type="text/css" href="<?php echo $doc_root . $app_path ?>assets/css/style.css" >
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css" >
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Charity Connection</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="assets/images/logo-blue-text.svg" class="logoNav" alt="Charity Connection Logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <?php if (isset($name)): ?>
+    
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <a class="nav-link nav_bar_link" href="../charity">Home</a>
@@ -38,6 +40,7 @@
         <li class="nav-item">
           <a class="nav-link nav_bar_link" href="../admin/about.php">About</a>
         </li>
+      <?php if (isset($name)): ?>
         <?php if ($name == 'admin'): ?>
           <li class="nav-item">
             <a class="nav-link nav_bar_link" href="?action=charity_sign_up">Sign Up Charity</a>
