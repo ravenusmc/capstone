4a60f0be05706b156294d98e47cbef46eff812f4
Liz <admin@elizabethmbank.info>
2018-10-09 16:09:40 -0400

    more work on stylesheet, added fonts, colors, and grid styles for landing page.

--- view/header.php
+++ view/header.php
@@ -21,16 +21,19 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://use.typekit.net/peb4ved.css">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css" >
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="assets/images/logo-blue-text.svg" class="logoNav" alt="Charity Connection Logo"></a>
+<nav class="navbar navbar-expand-lg navbar-light bg-yellow">
+  <a class="navbar-brand" href="index.php"><img src="assets/images/logo-blue-text.svg" class="logoNav" alt="Charity Connection Logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
+    Menu
   </button>
+  
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     
       <ul class="navbar-nav mr-auto">
