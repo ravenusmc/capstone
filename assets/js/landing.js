ed0d3604073255a911c32ca36e0bd7e73491e32e
Liz <admin@elizabethmbank.info>
2018-10-11 11:19:54 -0400

    continued to style landing page CTA buttons and added javascript for advanced search. Added advanced search select elements with all options based on the database information.

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,1 +1,12 @@
-alert('Hi');
+
+
+function showAdvSearch() {
+     var advSearchSection = document.getElementsByClassName('advSearchHidden');
+     
+     advSearchSection.style.visibility = "visible";
+}
+
+var advSearchBtn = document.getElementById('advSearchBtn');
+if (advSearchBtn.addEventListener) {                       advSearchBtn.addEventListener("click", showAdvSearch);
+ } else if (advSearchBtn.attachEvent) {                    advSearchBtn.attachEvent("onclick", showAdvSearch);
+ }
--- index.php
+++ index.php
@@ -19,43 +19,59 @@ include('view/header.php'); ?>
 
 <section class="homePageSection" id="problem">
      <h2 class="homePageHeading">What's the problem?</h2>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus ex ac luctus auctor. Ut sed orci pharetra, cursus enim eu, imperdiet tortor. Pellentesque ante turpis, condimentum eget augue nec, viverra suscipit neque. Nam id odio sed risus consectetur tincidunt. Maecenas ligula nulla, iaculis sed tempus eu, congue facilisis nulla. Vestibulum blandit finibus volutpat. In mattis, ipsum quis hendrerit mollis, sapien velit egestas mi, ac tincidunt odio est sed enim. Donec rhoncus, urna at elementum vehicula, lorem nulla sagittis ex, non ullamcorper leo nunc faucibus justo.</p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus ex ac luctus auctor. Ut sed orci pharetra, cursus enim eu, imperdiet tortor. Pellentesque ante turpis, condimentum eget augue nec, viverra suscipit neque. Nam id odio sed risus consectetur tincidunt. Maecenas ligula nulla, iaculis sed tempus eu, congue facilisis nulla. Vestibulum blandit finibus volutpat. In mattis, ipsum quis hendrerit mollis, sapien velit egestas mi, ac tincidunt odio est sed enim. </p>
 </section>
 
 <section class="homePageSection" id="solution">
      <h2 class="homePageHeading">How we solve it</h2>
-     <p>Nullam rhoncus nunc vitae dictum eleifend. Vestibulum faucibus lacus eget malesuada semper. Vivamus lacinia auctor enim vitae vestibulum. Duis cursus leo dictum, consequat nibh rutrum, bibendum sapien. Integer et euismod orci, vel interdum est. Integer in diam felis. Nullam eu scelerisque eros. Cras imperdiet est vel metus maximus, eu accumsan diam efficitur. Integer sed mi facilisis, blandit nulla nec, porttitor mi. Morbi dignissim eleifend quam, at euismod neque tempus ac. Proin cursus rhoncus sem porta congue. Nulla congue ultricies bibendum. Proin turpis nunc, ultricies quis malesuada eget, feugiat eu neque. Quisque mollis, lorem vel viverra lacinia, neque nisi pulvinar libero, nec porta sem magna eget nisl.</p>
+     <p>Nullam rhoncus nunc vitae dictum eleifend. Vestibulum faucibus lacus eget malesuada semper. Vivamus lacinia auctor enim vitae vestibulum. Duis cursus leo dictum, consequat nibh rutrum, bibendum sapien. Integer et euismod orci, vel interdum est. Integer in diam felis. Nullam eu scelerisque eros. Cras imperdiet est vel metus maximus, eu accumsan diam efficitur. Integer sed mi facilisis, blandit nulla nec, porttitor mi. Morbi dignissim eleifend quam, at euismod neque tempus ac. Proin cursus rhoncus sem porta congue.</p>
 </section>
 
 <section class="ctaSection" id="ctaSignUp">
      <h3 class="ctaHeading">Interested in donating?</h3>
-     <a href="admin/user_signup.php" class="ctaBtn">Sign up!</a>
+     <a href="admin/user_signup.php" class="ctaBtn btn">Sign up!</a>
 </section>
 
 <section class="ctaSection" id="ctaSearch"> 
-     <h3 class="ctaHeading">See charities in my area</h3>
+     <h3 class="ctaHeading">Find charities near your area</h3>
      <form action="charity/index.php" method="post" class="">
           <input type="hidden" name="action" value="see_charity_by_zip">
           <input type="text" name='zip' placeholder="Enter your zip code" class="" id="enterAddress">
-          <input type="submit" value="Go!">
+          <input type="submit" class="ctaBtn btn" value="Go!">
      </form>
-     <p class="advSearch"><a href="">Advanced Search</a></p>
+     <p class="advSearch btn" id="advSearchBtn">Advanced Search</p>
      <section class="advSearchHidden">
           <form action="" method="post" class="advSearchByType">
+          <label for="byType">Search by Charity Type</label>
                <select name="byType">
-                     <option value=""></option>
-                     <option value=""></option>
-                     <option value=""></option>
-                     <option value=""></option>
+                     <option value="education">Education</option>
+                     <option value="animals">Animals</option>
+                     <option value="artsculture">Arts/Culture</option>
+                     <option value="communitydev">Community Development</option>
+                     <option value="environment">Environment</option>
+                     <option value="health">Health</option>
+                     <option value="civilrights">Human and Civil Rights</option>
+                     <option value="humanservices">Human Services</option><option value="international">International</option>
+                     <option value="religion">Religious</option>
+                     <option value="research">Research/Public Policy</option>
+                     
                </select>
           </form>
 
-          <form action="" method="post" class="">
-               <select name="">
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
+          <form action="" method="post" class="advSearchByItems">
+            <label for="byItems">Search by Items Accepted by Charity</label>
+               <select name="byItems">
+                    <option value="household">Household Items</option>
+                    <option value="toys">Kids' Toys</option>
+                    <option value="kidclothes">Kids' Clothing</option>
+                    <option value="books">Books</option>
+                    <option value="appliances">Small Appliances</option>
+                    <option value="womenclothing">Women's Clothing</option>
+                    <option value="menclothing">Men's Clothing</option>
+                    <option value="womenshoes">Women's Shoes</option>
+                    <option value="menshoes">Men's Shoes</option>
+                    <option value="pets">Pet-related Items</option>
+                    <option value="garden">Yard or Garden Items</option>
                </select>
           </form>
 
