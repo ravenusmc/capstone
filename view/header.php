62f5ef5c8668e2f3e0455296244c59ee6b69548a
ravenusmc <mcuddy77@gmail.com>
2018-10-18 19:09:42 -0400

    added leafjs

--- view/header.php
+++ view/header.php
@@ -24,6 +24,14 @@
   <link rel="stylesheet" href="https://use.typekit.net/peb4ved.css">
   <link rel="stylesheet" type="text/css" href="assets/css/style.css" >
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  
+  <!-- LeafJS links -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+   crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+   integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+   crossorigin=""></script>
 </head>
 
 <body>
