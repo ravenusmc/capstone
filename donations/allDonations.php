94e985ab201db6e460d9bd97c355e6116af584f0
ravenusmc <mcuddy77@gmail.com>
2018-11-14 20:17:02 -0500

    fixed repeating date

--- donations/allDonations.php
+++ donations/allDonations.php
@@ -5,9 +5,13 @@ include '../view/header.php';
 
 <main class="page">
 <h2 class="pageHeading">All Donations</h2>
-     <?php foreach ($donations as $donation) : ?>
-          <h3><?php echo $donation['created']; ?></h3>
-          <h4 class="donationHeading">Donated Items</h4>
+    <?php $previous_date = $donation['created']; ?>
+    <?php foreach ($donations as $donation) : ?>
+      <?php if ($previous_date != $donation['created']): ?>
+        <h3><?php echo $donation['created']; ?></h3>
+        <h4 class="donationHeading">Donated Items:</h4>
+      <?php endif; ?>
+          <?php $previous_date = $donation['created'] ?>
           <p class="donationList"><?php echo $donation['items_list']; ?></p>
      <?php endforeach; ?>
 
